85808fb7364a131e2d137f1e15a44e17e5f780b7
Hugo Zhang <hugozzys@gmail.com>
2021-09-13 19:26:33 -0400

    add unit tests for stream implementation

--- tests/Socks5.Net.Extensions.UnitTests/RandomXorStreamTests.cs
+++ tests/Socks5.Net.Extensions.UnitTests/RandomXorStreamTests.cs
@@ -4,41 +4,35 @@ using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using FluentAssertions;
-using NSec.Cryptography;
-using NSec.Experimental;
 using Socks5.Net.Security;
 using Xunit;
 
 namespace Socks5.Net.Extensions.UnitTests
 {
-    public class Chacha20StreamTests
+    public class RandomXorStreamTests
     {
-        [Theory]
-        [InlineData(Crypto.Chacha20StreamBlockSize - 1)]
-        [InlineData(2*Crypto.Chacha20StreamBlockSize - 1)]
-        public async Task Async_NonIntegralMultipleOfBlockSize_ShouldSyncKeyStream(int byteChunk)
+        [Fact]
+        public async Task Async_ConsecutiveRead_ShouldSyncKeyStream()
         {
             // Arrange
-            var key = Key.Create(StreamCipherAlgorithm.ChaCha20);
-            var rand = new Random();
-            Memory<byte> nonce = new byte[Crypto.NonceFixedFieldSize];
-            rand.NextBytes(nonce.Span);
             var memStream = new MemoryStream();
-            var subjectUnderTests = new Chacha20Stream(memStream, key, nonce.Span, nonce.Span);
-            var testBytes = new byte[byteChunk];
-            var readBuffer = new byte[byteChunk];
+            var subjectUnderTests = new RandomXORStream(memStream,1,1);
+            var rand = new Random();
 
             // Act
             for (int i = 0; i < 10; ++i)
             {
+                var bytes = 1 << i;
+                var testBytes = new byte[bytes];
+                var readBytes = new byte[bytes];
                 rand.NextBytes(testBytes);
                 subjectUnderTests.Position = 0;
-                await subjectUnderTests.WriteAsync(testBytes[..byteChunk]);
+                await subjectUnderTests.WriteAsync(testBytes);
 
                 subjectUnderTests.Position = 0;
-                var readBytes = await subjectUnderTests.ReadAsync(readBuffer);
-                readBytes.Should().Be(byteChunk); 
-                readBuffer.Should().BeEquivalentTo(testBytes);
+                var readNum = await subjectUnderTests.ReadAsync(readBytes);
+                readNum.Should().Be(bytes); 
+                readBytes.Should().BeEquivalentTo(testBytes);
             }
         }
 
@@ -46,12 +40,8 @@ namespace Socks5.Net.Extensions.UnitTests
         public async Task Async_WriteOnce_ReadMultipleTimes_ShouldSyncKeyStream()
         {
             // Arrange
-            var key = Key.Create(StreamCipherAlgorithm.ChaCha20);
-            var rand = new Random();
-            Memory<byte> nonce = new byte[Crypto.NonceFixedFieldSize];
-            rand.NextBytes(nonce.Span);
             var mem = new MemoryStream();
-            var subjectUnderTests = new Chacha20Stream(mem, key, nonce.Span, nonce.Span);
+            var subjectUnderTests = new RandomXORStream(mem, 1, 1);
             List<int> readSequence = new List<int>(){63, 68, 129};
             var writeNum = readSequence.Aggregate(0, (acc, ele) => acc + ele);
             Memory<byte> writeBytes = new byte[writeNum];
@@ -69,32 +59,28 @@ namespace Socks5.Net.Extensions.UnitTests
             writeBytes.ToArray().Should().BeEquivalentTo(decryptedBytes.ToArray());
         }
 
-        [Theory]
-        [InlineData(Crypto.Chacha20StreamBlockSize - 1)]
-        [InlineData(2*Crypto.Chacha20StreamBlockSize - 1)]
-        public void Blocking_NonIntegralMultipleOfBlockSize_ShouldSyncKeyStream(int byteChunk)
+         [Fact]
+        public void Blocking_ConsecutiveRead_ShouldSyncKeyStream()
         {
             // Arrange
-            var key = Key.Create(StreamCipherAlgorithm.ChaCha20);
-            var rand = new Random();
-            Memory<byte> nonce = new byte[Crypto.NonceFixedFieldSize];
-            rand.NextBytes(nonce.Span);
             var memStream = new MemoryStream();
-            var subjectUnderTests = new Chacha20Stream(memStream, key, nonce.Span, nonce.Span);
-            var testBytes = new byte[byteChunk];
-            var readBuffer = new byte[byteChunk];
+            var subjectUnderTests = new RandomXORStream(memStream,1,1);
+            var rand = new Random();
 
             // Act
             for (int i = 0; i < 10; ++i)
             {
+                var bytes = 1 << i;
+                var testBytes = new byte[bytes];
+                var readBytes = new byte[bytes];
                 rand.NextBytes(testBytes);
                 subjectUnderTests.Position = 0;
-                subjectUnderTests.Write(testBytes[..byteChunk]);
+                subjectUnderTests.Write(testBytes);
 
                 subjectUnderTests.Position = 0;
-                var readBytes = subjectUnderTests.Read(readBuffer);
-                readBytes.Should().Be(byteChunk); 
-                readBuffer.Should().BeEquivalentTo(testBytes);
+                var readNum = subjectUnderTests.Read(readBytes);
+                readNum.Should().Be(bytes); 
+                readBytes.Should().BeEquivalentTo(testBytes);
             }
         }
 
@@ -102,12 +88,8 @@ namespace Socks5.Net.Extensions.UnitTests
         public void Blocking_WriteOnce_ReadMultipleTimes_ShouldSyncKeyStream()
         {
             // Arrange
-            var key = Key.Create(StreamCipherAlgorithm.ChaCha20);
-            var rand = new Random();
-            Span<byte> nonce = new byte[Crypto.NonceFixedFieldSize];
-            rand.NextBytes(nonce);
             var mem = new MemoryStream();
-            var subjectUnderTests = new Chacha20Stream(mem, key, nonce, nonce);
+            var subjectUnderTests = new RandomXORStream(mem, 1, 1);
             List<int> readSequence = new List<int>(){63, 68, 129};
             var writeNum = readSequence.Aggregate(0, (acc, ele) => acc + ele);
             Span<byte> writeBytes = new byte[writeNum];
